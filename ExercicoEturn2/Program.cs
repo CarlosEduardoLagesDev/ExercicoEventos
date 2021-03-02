8b3ed45320dca9ec38c0c4d50b5bd5170c4b42b8
Cadu Lages Souza <cadulages1@gmail.com>
2021-03-02 19:06:59 -0300

    Add project files.

---
+++ ExercicoEturn2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicoEturn2
+{
+    class Program
+    {
+        static TimeSpan InicioAlmoco = TimeSpan.Parse("12:00");
+        static TimeSpan TerminoAlmoco = TimeSpan.Parse("13:00");
+        static TimeSpan InicioNetworking = TimeSpan.Parse("17:00");
+        static TimeSpan InicioEvento = TimeSpan.Parse("09:00");
+
+        static void Main(string[] args)
+        {
+            
+            Dictionary<int, string> Dicionario = new Dictionary<int, string>();
+            string Nome;
+            int duracao;
+
+            //Main Aplication
+            {
+                int totalAcumuladoMinutosEvento = 0;
+                do
+                {
+                    Console.WriteLine("Digite o nome do Evento");
+                    Nome = Console.ReadLine();
+                    Console.WriteLine("Digite a duração");
+                    duracao = Convert.ToInt32(Console.ReadLine());
+
+                    if (totalAcumuladoMinutosEvento + duracao <= 840)
+                    {
+                        Dicionario.Add(duracao, Nome);
+                        totalAcumuladoMinutosEvento += Convert.ToInt32(duracao);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Limite de tempo excedido");
+                    }
+
+                } while (totalAcumuladoMinutosEvento < 840);
+            }
+
+            TimeSpan AddHoraInicio = InicioEvento.Add(TimeSpan.FromMinutes(duracao));
+
+
+
+
+            //Print
+            {
+                   
+            }
+            
+            
+            
+
+
+            
+
+        }
+            
+
+
+        
+    }
+}
